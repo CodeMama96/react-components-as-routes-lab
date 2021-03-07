6d5e3a9ee2f7b7e1fcb4d78f210ecea7784b8b3c
Alicia Weenum <weenum.alicia@gmail.com>
2021-03-06 20:29:17 -0500

    commit tree 98964d804a02d15e8c8d9da0707808e290c8a323

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,6 +5,16 @@ const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index)=>(
+        <div className='actor' key={index}>
+          <h3>{actor.name}</h3>
+          <p>{actor.movies}</p>
+
+        </div>
+      )
+      )}
+      
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,18 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index)=>(
+        <div className='movie' key={index}>
+          <h3>{movie.title}</h3>
+          <p>{movie.time}</p>
+          <p>{movie.genres}</p>
+          <p>{movie.metascore}</p>
+
+        </div>
+        )
+      )}
+
     </div>
   );
 };
